bbe8313dd62832f12bffb88f6ec84056478ceab7
QL358 <qiangluo.private@gmail.com>
2025-11-19 12:35:41 +0100

    Add Basic JSON storage module

--- storage.py
+++ storage.py
@@ -1,1 +1,33 @@
-# take input.py and save to json or csv. take json or csv and turn into output for analysis.py
+# take cli.py and save to json. take json and turn into output for core.py
+
+import json
+import os
+from pathlib import Path
+
+DATA_FILE = Path("save.json")
+
+def savelst(lst):
+    with DATA_FILE.open("w", encoding="utf-8") as f:
+        json.dump(lst, f, indent=4)
+
+def loadlst():
+    if DATA_FILE.exists() == False:
+        return(False) # no save yet
+    else:
+        with DATA_FILE.open("r", encoding="utf-8") as f:
+            return json.load(f) # returns list of dicts
+        
+def delsavereq():
+    if DATA_FILE.exists() == False:
+        return(False) # no save 
+    else:
+        os.remove("save.json")
+        return(True) # successful
+
+
+if __name__ == "__main__":
+    from core import testin
+    lst = testin()
+    savelst(lst)
+    print("1")
+    print(delsavereq())
