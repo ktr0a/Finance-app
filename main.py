2d425adc3190e2931ef39879aed0cecf4bd691fe
ktr0a <qiangluo.private@gmail.com>
2025-11-25 18:33:58 +0100

    refactor: simplify main function

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from cli import start, prehub, hub
+from cli.cli import start, prehub, hub
 
 if __name__ == "__main__":
-    choice = start()
-    save = prehub(choice)
-    hub(save)
+    hub(prehub(start()))
